beffb88fd5fccce1896d01df813df4cc31509534
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2023-10-21 01:22:28 +0800

    0.0.1 Separated main.py & strategy.py

--- main.py
+++ main.py
@@ -2,66 +2,7 @@ import datetime
 
 import backtrader as bt
 
-class sample_strategy(bt.Strategy):
-    def log(self, txt, dt=None):
-        ''' Logging function for this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-        
-    def __init__(self):
-        # self.datas: feed to the strategy
-        self.dataclose = self.datas[0].close
-        # To keep track of pending orders
-        self.order = None
-
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
-            return
-
-        # Check if an order has been completed
-        # Attention: broker could reject order if not enough cash
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
-            elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
-
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        # Write down: no pending order
-        self.order = None
-    
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
-                 (trade.pnl, trade.pnlcomm))
-    
-    def next(self):
-        #self.log('Close, %.2f' % self.dataclose[0])
-        if self.order: # there is an open order
-            return
-        # Check if we are in the market
-        if not self.position.size: # not in position! can buy!
-            if self.dataclose[0] < self.dataclose[-1]:
-                if self.dataclose[-1] < self.dataclose[-2]:
-                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                    # Keep track of the created order to avoid a 2nd order
-                    self.order = self.buy(size = 25)
-
-        else:
-            # Already in the market ... we might sell
-            if len(self) >= (self.bar_executed + 5):
-                # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell(size = 25)
+from strategy import sample_strategy
 
 if __name__ == '__main__':
     # initialize driver & add strategy
